af4c6b92e3fa4b566ef4cf9e79cef163b6fa4d7b
zufogocoding <phamthanhnhuttrong123456@gmail.com>
2026-04-21 11:30:42 +0700

    Adding Package for API song upload

--- db/index.js
+++ db/index.js
@@ -3,23 +3,21 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  host: 'localhost',
+  host: process.env.DB_HOST || 'localhost',
   port: 5432,
 });
-async function run() {
-  try {
-    await Pool.connect();
-    console.log('Success connect to database: ${process.env.DB_NAME}');
-
-  } catch (err) {
-    console.error("Error connect");
-  } finally {
-    await Pool.end();
-    console.log("Close the connect");
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error("Error connect to PostgreSQL:", err.stack);
+  } else {
+    console.log('Connected to database: ${process.env.DB_NAME}');
+    release();
   }
-}
+});
 module.exports = {
   query: (text, params) => pool.query(text, params),
   pool,
 };
+
+
 
