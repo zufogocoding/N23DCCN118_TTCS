49ee0d9b69c087ad858164e5cef8de01c59bee52
zufogocoding <phamthanhnhuttrong123456@gmail.com>
2026-04-21 11:49:30 +0700

    Expose folder

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('Hello from PandaExpress')
 })
-
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.listen(9000, '0.0.0.0', () => {
   console.log('The server is now live and pretty much acessable')
 })
