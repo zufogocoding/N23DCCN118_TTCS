cb9c16767cf905a58839631c3296947bd1e3708f
zufogocoding <phamthanhnhuttrong123456@gmail.com>
2026-05-05 12:58:51 +0700

    fixing around but completely fail and suggest new solution

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Needed for Docker
+    port: 5173,
+    watch: {
+      usePolling: true // Enable this if hot-reload isn't working in Docker
+    }
+  }
 })
