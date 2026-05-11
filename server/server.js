3474122bb4ee90483276d11ed91ae4ade009a61b
zufogocoding <phamthanhnhuttrong123456@gmail.com>
2026-05-11 18:02:11 +0700

    mounted

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const songRoutes = require('./routes/songRoutes.js')
 const authRoutes = require('./routes/authRoutes.js')
 const playlistRoutes = require('./routes/playlistRoutes.js');
 const interactRoutes = require('./routes/interactRoutes.js');
+const dashboardRoutes = require('./routes/dashboardRoutes.js');
 const app = express()
 
 app.use(cors()); // Cho phép Frontend gọi API 
@@ -22,6 +23,8 @@ app.use(songRoutes);
 app.use(streamRoutes);
 app.use(authRoutes);
 app.use(playlistRoutes);
+app.use(interactRoutes);
+app.use(dashboardRoutes);
 
 app.listen(9000, '0.0.0.0', () => {
   console.log('The server is now live and pretty much acessable')
