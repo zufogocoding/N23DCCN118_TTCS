4187dfdbb4431360f5803045b3accfde4bab5b81
Tuanka1 <n23dccn134@student.ptithcm.edu.vn>
2026-05-21 00:55:17 +0700

    chart ở dashboard

--- server/routes/dashboardRoutes.js
+++ server/routes/dashboardRoutes.js
@@ -5,6 +5,8 @@ const dashboardController = require('../controllers/dashboardController');
 
 router.get('/api/dashboard/stats', dashboardController.getStats);
 router.get('/api/dashboard/users', dashboardController.getUsers);
-
 router.get('/api/dashboard/songs', dashboardController.getSongs);
+router.get('/api/dashboard/streaming-stats', dashboardController.getStreamingStats);
+router.get('/api/dashboard/recent-activities', dashboardController.getRecentActivities);
+
 module.exports = router;
