763cb357972eb5c86cf264571834c9629d798024
zufogocoding <phamthanhnhuttrong123456@gmail.com>
2026-05-21 01:20:32 +0700

    useState probably react hook

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import SongDetail from "./pages/music/SongDetail";
 import PlaylistView from './pages/music/PlaylistView';
